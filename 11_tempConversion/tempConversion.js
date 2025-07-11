3f6fdcfa1824349049589e0ce5bea0f44cbb2dec
radoslavkostov <103312630+radoslavkostov@users.noreply.github.com>
2025-07-11 06:53:43 +0300

    Solve exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitGrades) {
+  return Number(((fahrenheitGrades - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusGrades) {
+  return Number((celsiusGrades * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
