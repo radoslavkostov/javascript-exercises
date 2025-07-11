40d8de22b0ce1fe3e008f115f7ccc3f4342e69c7
radoslavkostov <103312630+radoslavkostov@users.noreply.github.com>
2025-07-11 06:32:47 +0300

    Solve exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0 || Math.floor(a) !== a || Math.floor(b) !== b) {
+        return "ERROR";
+    }
+    if(b < a) {
+        [a, b] = [b, a];
+    }
 
+    let res = 0;
+    for(let i = a; i <= b; ++i) {
+        res += i;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
